57c50757011598bb5fddb684731c122fe8c10568
Sam Hollyer <rsamhollyer@gmail.com>
2022-04-15 08:01:00 -0500

    update

--- screenlayout/docked.sh
+++ screenlayout/docked.sh
@@ -1,7 +1,27 @@
 #!/bin/sh
-THIRD_MONITOR=$( xrandr -q | rg " connected" | rg ^DisplayPort | awk '{print $1}' )
-xrandr --output eDP --off --output HDMI-A-0 --primary --mode 2560x1440 --rate 143.86 --pos 0x0 --rotate normal --output "$THIRD_MONITOR" --mode 2560x1440 --pos 2560x0 --rotate normal --rate 119.88 -v
 
-pgrep -x sxhkd > /dev/null || sxhkd &
-picom --experimental-backends -b  &
-bspc wm -r
+monitor_count="$(xrandr -q | rg " connected" | wc -l)"
+all_monitors=($(xrandr -q | rg " connected" | awk '{print $1}'))
+
+echo "$monitor_count"
+echo "$all_monitors"
+
+case $monitor_count in
+3)
+    xrandr --output "${all_monitors[0]}" --off --output "${all_monitors[1]}" --primary --mode 2560x1440 --rate 143.86 --pos 0x0 --rotate normal --output "${all_monitors[2]}" --mode 2560x1440 --pos 2560x0 --rotate normal --rate 119.88
+
+    ;;
+2)
+
+    xrandr --output "${all_monitors[0]}" --mode 1920x1080 --pos 0x0 --output "${all_monitors[1]}" --primary --mode 2560x1440 --pos 1920x0 --rotate normal
+
+    ;;
+1)
+    xrandr --output "${all_monitors[0]}" --mode 1920x1080 --rate 60.01 --pos 0x0 --rotate normal
+
+    ;;
+esac
+    pgrep -x sxhkd > /dev/null || sxhkd &
+    picom --experimental-backends -b  &
+    bspc wm -r
+    xmodmap ~/.Xmodmap
--- screenlayout/mons-plug.sh
+++ screenlayout/mons-plug.sh
@@ -1,38 +1,24 @@
 #!/bin/sh
 
+all_monitors=($(xrandr -q | rg " connected" | awk '{print $1}'))
+
 case ${MONS_NUMBER} in
     3)
-    extra="$(xrandr -q | rg " connected" | rg ^DisplayPort | awk '{print $1}')"
-    xrandr --output eDP --off --output HDMI-A-0 --primary --mode 2560x1440 --rate 143.86 --pos 0x0 --rotate normal --output "$extra" --mode 2560x1440 --pos 2560x0 --rotate normal --rate 119.88
-
-    pgrep -x sxhkd > /dev/null || sxhkd &
-    picom --experimental-backends -b  &
-    bspc wm -r
-    xmodmap ~/.Xmodmap
-    sh ~/.config/wpg/wp_init.sh
+    xrandr --output "${all_monitors[0]}" --off --output "${all_monitors[1]}" --primary --mode 2560x1440 --rate 143.86 --pos 0x0 --rotate normal --output "${all_monitors[2]}" --mode 2560x1440 --pos 2560x0 --rotate normal --rate 119.88
 
     ;;
     2)
-    extra="$(xrandr -q | rg " connected" | rg ^DisplayPort | awk '{print $1}')"
-    xrandr --output eDP --mode 1920x1080 --rate 60.01 --pos 0x0 --rotate normal --output "${extra:-HDMI-A-0}" --primary --mode 2560x1440 --rate 143.86 --pos 1920x0 --rotate normal
 
-    pgrep -x sxhkd > /dev/null || sxhkd &
-    picom --experimental-backends -b  &
-    bspc wm -r
-    xmodmap ~/.Xmodmap
-    sh ~/.config/wpg/wp_init.sh
+    xrandr --output "${all_monitors[0]}" --mode 1920x1080 --pos 0x0 --output "${all_monitors[1]}" --primary --mode 2560x1440 --pos 1920x0 --rotate normal
 
     ;;
     1)
-    xrandr --output eDP --mode 1920x1080 --rate 60.01 --pos 0x0 --rotate normal
-
-    pgrep -x sxhkd > /dev/null || sxhkd &
-    picom --experimental-backends -b  &
-    bspc wm -r
-    xmodmap ~/.Xmodmap
-    sh ~/.config/wpg/wp_init.sh
+    xrandr --output "${all_monitors[0]}" --mode 1920x1080 --rate 60.01 --pos 0x0 --rotate normal
 
     ;;
 esac
-
+    pgrep -x sxhkd > /dev/null || sxhkd &
+    pgrep -x picom > /dev/null || picom --experimental-backends -b  &
+    bspc wm -r
+    xmodmap ~/.Xmodmap
 

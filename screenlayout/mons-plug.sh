4eaf5f97a1f52a4a83a58a9a4a2e5a518cb5a531
Sam Hollyer <rsamhollyer@gmail.com>
2022-05-09 15:32:29 -0500

    ruddr hotfixed their 500 errors

--- screenlayout/mons-plug.sh
+++ screenlayout/mons-plug.sh
@@ -5,10 +5,22 @@ HDMI=$(xrandr -q | grep 'HDMI-A-0 connected' | awk '{print $1}')
 DP=$(xrandr -q | grep 'DisplayPort-[0-9] connected' | awk '{print $1}')
 
 
+function disconnect_dp(){
+    active_monitors=($(xrandr --listactivemonitors | grep 'DisplayPort-[0-9]' | awk '{print $4}'))
+    connect_monitors=($(xrandr -q | grep 'DisplayPort-[0-9] connected' | awk '{print $1}'))
+
+
+    for i in "${active_monitors[@]}"; do
+        [[ $i != "${connect_monitors[0]}" ]] && echo "Disconnecting $i" && xrandr --output "$i" --off
+    done
+}
+
+
 case ${MONS_NUMBER} in
 3)
     xrandr --output "$eDP" --off --output "$HDMI" --primary --mode 2560x1440 --pos 0x0 --rotate normal --output "$DP" --mode 2560x1440 --pos 2560x0 --rotate normal
 
+    disconnect_dp
     ;;
 2)
 

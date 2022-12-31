01d9984d678850fb944dc8be4e7cf24b8329485a
Sam Hollyer <rsamhollyer@gmail.com>
2022-12-31 06:07:05 -0600

    updates

--- screenlayout/.config/screenlayout/docked.sh
+++ screenlayout/.config/screenlayout/docked.sh
@@ -6,11 +6,7 @@ HDMI=$(xrandr -q | grep 'HDMI-A-0 connected' | awk '{print $1}')
 DP=$(xrandr -q | grep 'DisplayPort-[0-9] connected' | awk '{print $1}')
 
 function disconnect_dp() {
-  xrandr --output DisplayPort-0 --off
-  xrandr --output DisplayPort-1 --off
-  xrandr --output DisplayPort-2 --off
-  xrandr --output DisplayPort-3 --off
-  xrandr --output DisplayPort-4 --off
+  xrandr --output DisplayPort-0 --off --output DisplayPort-1 --off --output DisplayPort-2 --off --output DisplayPort-3 --off --output DisplayPort-4 --off --output DisplayPort-5 --off
 
 }
 
--- screenlayout/.config/screenlayout/mons-plug.sh
+++ screenlayout/.config/screenlayout/mons-plug.sh
@@ -5,11 +5,7 @@ HDMI=$(xrandr -q | grep 'HDMI-A-0 connected' | awk '{print $1}')
 DP=$(xrandr -q | grep 'DisplayPort-[0-9] connected' | awk '{print $1}')
 
 function disconnect_dp() {
-  xrandr --output DisplayPort-0 --off
-  xrandr --output DisplayPort-1 --off
-  xrandr --output DisplayPort-2 --off
-  xrandr --output DisplayPort-3 --off
-  xrandr --output DisplayPort-4 --off
+  xrandr --output DisplayPort-0 --off --output DisplayPort-1 --off --output DisplayPort-2 --off --output DisplayPort-3 --off --output DisplayPort-4 --off --output DisplayPort-5 --off
 
 }
 
@@ -17,11 +13,11 @@ disconnect_dp
 
 case ${MONS_NUMBER} in
 3)
-  xrandr --output "$eDP" --off --output "$HDMI" --primary --mode 2560x1440 --pos 0x0 --rotate normal --output "$DP" --mode 2560x1440 --pos 2560x0 --rotate normal
+  xrandr --output "$eDP" --off --output "$HDMI" --primary --mode 2560x1440 --rate 143.86 --pos 0x0 --rotate normal --output "$DP" --mode 2560x1440 --pos 2560x0 --rotate normal
 
   ;;
 2)
-  xrandr --output "$eDP" --mode 1920x1080 --pos 0x0 --output "$HDMI" --primary --mode 2560x1440 --pos 1920x0 --rotate normal
+  xrandr --output "$eDP" --mode 1920x1080 --pos 0x0 --output "$HDMI" --primary --mode 2560x1440 --rate 143.86 --pos 0x0 --rotate normal
 
   ;;
 
